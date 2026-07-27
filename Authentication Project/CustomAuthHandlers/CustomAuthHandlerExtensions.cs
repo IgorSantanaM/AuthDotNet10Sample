7656bd4b71e1ebd1c24c3fff32c1f4dd68c14ad8
Igor Santana Medeiros <1434@ucdb.br>
2026-07-27 15:31:06 -0400

    (feat): Added auth schemes

---
+++ Authentication Project/CustomAuthHandlers/CustomAuthHandlerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace StartcodeAuthentication.CustomAuthHandlers
+{
+    public static class CustomAuthHandlerExtensions
+    {
+        public static AuthenticationBuilder AddCustomAuth(
+            this AuthenticationBuilder builder,
+            string authenticationScheme,
+            string? displayName,
+            Action<CustomAuthHandlerOptions> configureOptions)
+        {
+            return builder.AddScheme<CustomAuthHandlerOptions, CustomAuthHandler>(
+                authenticationScheme,
+                displayName,
+                configureOptions);
+        }
+    }
+}
