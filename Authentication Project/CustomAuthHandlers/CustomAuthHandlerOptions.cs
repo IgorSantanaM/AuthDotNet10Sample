e08f93aaa73d888896525266c0d8df8341298717
Igor Santana Medeiros <1434@ucdb.br>
2026-07-27 14:38:24 -0400

    (feat): Added Auth Operations

---
+++ Authentication Project/CustomAuthHandlers/CustomAuthHandlerOptions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace StartcodeAuthentication.CustomAuthHandlers
+{
+    public class CustomAuthHandlerOptions : AuthenticationSchemeOptions
+    {
+        public static string DefaultAuthenticationScheme => "CustomAuthHandler";
+        public string LoginPath { get; set; } = "/User/Login";
+        public string CookieName { get; set; } = "AuthCookie";
+        public string DefaultRedirectPath { get; set; } = "/AuthTest";
+        public string AccessDeniedPath { get; set; } = "/User/AccessDenied";
+    }
+}
